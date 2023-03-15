1c631d2f98b5c0518d225b34922916c7522d9dd4
AkshatDasondhi <85483154+AkshatDasondhi@users.noreply.github.com>
2023-03-15 14:00:00 +0100

    Custom Component

---
+++ JavaBeans/CustomComponent.java
@@ -0,0 +1,74 @@
+package Beans;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class CustomComponent extends JComponent {
+    private int x, y, width, height;
+    private Color color;
+
+    public CustomComponent() {
+        x = 0;
+        y = 0;
+        width = 50;
+        height = 50;
+        color = Color.RED;
+    }
+
+    public int getx() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+        repaint();
+    }
+
+    public int gety() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+        repaint();
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+        repaint();
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+        repaint();
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+        repaint();
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(width, height);
+    }
+
+    @Override 
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(color);
+        g.fillRect(x, y, width, height);
+    }
+}
