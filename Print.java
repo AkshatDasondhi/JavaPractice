e5ac765cc17b7405c26fd9f374b3c4ee463e936d
AkshatDasondhi <85483154+AkshatDasondhi@users.noreply.github.com>
2023-03-22 00:16:23 +0530

    newupdate

--- Print.java
+++ Print.java
@@ -5,6 +5,6 @@ public class Print {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.println(n);   
-        
+        System.out.println("hello");
     }
 }
